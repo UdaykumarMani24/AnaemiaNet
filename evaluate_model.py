a0b7657060cfbdac0d33c0e4097206acfeca3291
UdaykumarMani24 <121280684+UdaykumarMani24@users.noreply.github.com>
2026-06-14 07:22:59 +0530

    Create evaluate_model.py

---
+++ evaluate_model.py
@@ -0,0 +1,21 @@
+"""
+Evaluation Script
+
+Computes:
+
+* ROC
+* AUC
+* Sensitivity
+* Specificity
+* Accuracy
+* Precision
+* Recall
+* F1
+* Confusion Matrix
+
+Threshold should be selected using validation data.
+
+Outputs:
+metrics.json
+roc_curve.png
+"""

c6d13306d62357fa4e231dcfb4ee4a1ce553cd98
UdaykumarMani24 <121280684+UdaykumarMani24@users.noreply.github.com>
2026-06-14 07:23:40 +0530

    Create export_tflite.py

---
+++ export_tflite.py
@@ -0,0 +1,15 @@
+"""
+TensorFlow Lite Export
+
+Exports trained model for mobile inference.
+
+Supported:
+
+* FP16
+* INT8
+
+Output:
+model.tflite
+
+Deployment assets are not included in public release.
+"""
